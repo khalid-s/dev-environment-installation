02966acb07522346ea41a6a86aef0db7cf734ecc
Khalid Sookia <khalidsookia@gmail.com>
2016-07-08 14:16:56 +0200

    initialized dev environment installer

---
+++ README.md
@@ -0,0 +1,39 @@
+Dev environment installation
+============================
+
+The installer will install the following:
+* tasksel
+* lamp-server
+* php5-curl curl php5-gd php5-cgi php5-intl
+* phpmyadmin
+* composer
+* ruby-full
+* compass
+* nodejs-legacy
+* npm
+* bower
+* grunt
+
+## If your environment is not configured you can use the following bash file to install the required packages for this course:
+
+```
+git clone https://github.com/khalid-s/dev-environment-installation.git ~
+```
+
+Go to the installation folder
+```
+cd ~/dev-environment-installation
+```
+
+Then launch the install bash file 
+```
+./install.sh
+```
+
+during the installation process set a password equal to 'paris' for MySQL
+
+## This installation process is for first time installation on  fresh ubuntu.
+
+For ease purpose set all passwords to paris. You will be able to change your password later for production.
+
+If you choose another password for mysql then be careful not to take the default password paris when the app is being installed
---
+++ install.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+sudo locale-gen "fr_FR.UTF-8"
+sudo dpkg-reconfigure locales
+
+# Shell color variables
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+lightgrey=`tput setaf 6`
+reset=`tput sgr0`
+
+sudo mkdir -p /var/www/html
+
+cd /var/www
+
+# Resolved rights problems
+sudo groupadd iknsa
+sudo usermod -a -G iknsa www-data
+sudo usermod -a -G iknsa $(whoami)
+sudo chown -R $(whoami):iknsa /var/www
+sudo chgrp -R iknsa /var/www
+
+# Make sure kernel is up to date before getting started
+sudo apt-get update --fix-missing
+sudo apt-get upgrade
+
+# Install lamp-server PHP Apache MySQL
+printf "${yellow}Looks like php is not installed. We'll install the whole lamp-server stack with tasksel${reset}\n"
+sudo apt-get install tasksel
+sudo tasksel install lamp-server
+
+# Install ruby
+sudo apt-get install ruby-full
+
+# Install sass and compass
+printf "${yellow}Looks like compass is not installed. We'll install compass using gem install compass${reset}\n"
+sudo gem update
+sudo gem install compass
+
+printf "${green}We are now going to install a few stuffs for php${reset}\n"
+# Install Curl and php5-curl (php5-curl gets enabled automatically)
+# Install php5-cgi to launch install/index.php with params
+sudo apt-get install php5-curl curl php5-gd php5-cgi php5-intl
+
+printf "${yellow}We are now going to install phpmyadmin${reset}\n"
+# Installing phpmyadmin as it is useful for devs
+sudo apt-get install phpmyadmin
+
+# Composer bug lack of memory
+# https://getcomposer.org/doc/articles/troubleshooting.md#proc-open-fork-failed-errors
+sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+sudo /sbin/mkswap /var/swap.1
+sudo /sbin/swapon /var/swap.1
+
+# Composer installation
+printf "${yellow}Composer is not installed globally... We'll try to get it done :)${reset}\n"
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+if ! [ -x "$(command -v unzip)" ]; then
+    printf "${yellow}Unzip is not installed. We'll install it using sudo apt-get install unzip :)${reset}\n"
+    sudo apt-get install unzip
+fi
+
+# npm install bug
+# This is a temporary fix as it will be fixed in npm#2.7
+mkdir -p /home/$(whoami)/tmp
+sudo chmod -R 777 /home/$(whoami)/tmp
+# @todo to remove this hack as it may present a security issue
+
+# Nodejs and npm installation
+printf "${yellow}Looks like npm and nodejs are not installed... We'll give it a shot.${reset}\n"
+sudo apt-get install npm nodejs-legacy
+
+# Grunt and bower install
+printf "${yellow}We are now going to install bower and grunt-cli globally${reset}\n"
+
+# Install bower if was not installed
+sudo npm install -g bower
+# Symbolic link for bower as sometimes bugs
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+
+# Install grunt-cli if was not installed
+sudo npm install -g grunt-cli
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
+# Install dependencies
+# Checks that bower installed properly
+if ! [ -x "$(command -v bower)" ]; then
+    printf "${red}Ooops... Bower did not installed properly${reset}\n"
+else
+    printf "${yellow}Installing bower dependencies${reset}\n"
+    bower install
+fi
+
+# Checks that grunt properly installed
+printf "${red}Ooops... Grunt did not installed properly${reset}\n"
+printf "${yellow}We are now going to install npm dependencies for the project with npm install${reset}\n"
+npm install
